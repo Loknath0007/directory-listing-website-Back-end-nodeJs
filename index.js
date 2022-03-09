392be76cb96fd21852bcfd597ec7068edb837038
Iamzayed007 <zhr1720@gmail.com>
2022-03-09 11:53:10 +0600

    update

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 require('dotenv').config();
+
+
+const { MongoClient } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('server Running')
+  })
+  
+  app.listen(port, () => {
+    console.log(`listening at ${port}`)
+  })

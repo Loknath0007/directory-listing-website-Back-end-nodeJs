e4b58b3ba40fcd3b972db173876f139f09eca4b9
unknown <mntanveer9196@gmail.com>
2022-05-22 12:57:42 +0600

    category CRUD

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const errorHandler = require('./middlewares/error');
 const connectDB = require('./config/db');
 // const sessoin = require('e')
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 // const mongoose = require('mongoose')
 const mongoose = require('mongoose');
 
@@ -121,9 +121,13 @@ app.use((err, req, res, next) => {
   }
 });
 
-mongoose.connection.once('open', () => {
-  console.log('connect to mongo');
-  app.listen(port, () => {
-    console.log(`listening at ${port}`);
-  });
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });

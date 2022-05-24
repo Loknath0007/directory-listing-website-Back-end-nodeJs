8e4ce55418043c0dfc8de254915f7fba96c0f83a
ImtiazMahmod <imtiazmahmod2@gmail.com>
2022-05-24 11:53:17 +0600

    auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,16 +9,7 @@ const asyncHandler = require("../middlewares/async");
 const userRegister = asyncHandler(async (req, res) => {
   const newUser = await User.create(req.body);
 
-  const data = {
-    user: {
-      id: newUser.id,
-    },
-  };
-
-  const authToken = await jwt.sign(
-    { name: newUser.name, id: newUser.id },
-    process.env.JWT_SECRET
-  );
+  const authToken = await jwt.sign({ id: newUser.id }, process.env.JWT_SECRET);
   res.status(200).json({
     success: true,
     authToken,
@@ -27,47 +18,37 @@ const userRegister = asyncHandler(async (req, res) => {
 });
 
 // user login
-const userLogin = async (req, res) => {
-  try {
-    const user = await User.find({ email: req.body.email });
-    console.log(user);
-    if (user) {
-      const isValid = await bcrypt.compare(req.body.password, user[0].password);
-      if (isValid) {
-        //generate token
-        const authToken = jwt.sign(
-          {
-            name: user[0].name,
-            id: user[0]._id,
-          },
-          process.env.JWT_SECRET,
-          {
-            expiresIn: "24h",
-          }
-        );
-
-        // localStorage.setItem('username'=req.body.username)
-
-        res.status(200).json({
-          access_token: authToken,
-          message: "Login successful",
-        });
-      } else {
-        res.status(401).json({
-          message: "Authentication Failed",
-        });
-      }
-    } else {
-      res.status(401).json({
-        message: "Authentication Failed",
-      });
-    }
-  } catch (error) {
-    res.status(401).json({
-      message: "Authentication Failed",
-    });
+const userLogin = asyncHandler(async (req, res, next) => {
+  const user = await User.find({ email: req.body.email });
+  console.log(user);
+  if (!user) {
+    return next(new ErrorResponse(`Invalid Email or Password`, 404));
   }
-};
+
+  const isValid = await bcrypt.compare(req.body.password, user[0].password);
+
+  if (!isValid) {
+    return next(new ErrorResponse(`Invalid Email or Password`, 404));
+  }
+
+  //generate token
+  const authToken = jwt.sign(
+    {
+      id: user[0]._id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "24h",
+    }
+  );
+
+  // localStorage.setItem('name'=req.body.username)
+
+  res.status(200).json({
+    access_token: authToken,
+    message: "Login successful",
+  });
+});
 
 const userRoleChecking = async (req, res) => {
   try {
